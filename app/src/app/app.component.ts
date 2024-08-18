2c7686af4dffa406817f3c89e421d6970c97b775
Hamilton Lima <hamilton.lima@gmail.com>
2024-08-18 16:29:34 -0400

    First version of app with bulma installed

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VideoPreviewComponent } from './video-preview/video-preview.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VideoPreviewComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'app';
