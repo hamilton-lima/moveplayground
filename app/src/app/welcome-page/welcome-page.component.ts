ae8d4b80d2c153638ec3ee07527e340be28de9f4
Hamilton Lima <hamilton.lima@gmail.com>
2024-08-26 23:49:06 -0400

    Hides navigation until is ready

--- app/src/app/welcome-page/welcome-page.component.ts
+++ app/src/app/welcome-page/welcome-page.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, CommonModule],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.scss',
 })
 export class WelcomePageComponent {
+  ready = false;
   constructor(private router: Router) {}
 
   balloonsGame() {
