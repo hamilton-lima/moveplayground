fdde4302e533d714fb8f45099212551ec58372b2
Hamilton Lima <hamilton.lima@gmail.com>
2024-08-26 23:09:16 -0400

    Adds not found route

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { BalloonsGamePageComponent } from './balloons-game-page/balloons-game-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 export const routes: Routes = [
   { path: '', component: WelcomePageComponent }, // Default route
   { path: 'balloons', component: BalloonsGamePageComponent }, // /balloons route
+  { path: '*', component: NotFoundPageComponent }, // /balloons route
 ];
