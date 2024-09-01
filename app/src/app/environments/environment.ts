3b8ef7c56d6d75fde40e6e3a6b38ed1300a34c2f
Hamilton Lima <hamilton.lima@gmail.com>
2024-09-01 12:15:05 -0400

    Adds empty supabase authentication service

--- app/src/app/environments/environment.ts
+++ app/src/app/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
   mixpanelApiKey: '8cc569063cd6b9fcfdf082b96e6e5304',
+  supabaseApiKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4cXZ5b3R5bGVzY3F4YWF5cWJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUyMDU3NjMsImV4cCI6MjA0MDc4MTc2M30.ypdZbKEQX7iZR8t6EoWFIJJj5sktkQGsNAt7kIjWkJk',
+  supabaseProjectURL: 'https://hxqvyotylescqxaayqbl.supabase.co',
 };
