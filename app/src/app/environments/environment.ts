d7a22e24da3469e4605912e0211da275cb87008b
Hamilton Lima <hamilton.lima@gmail.com>
2024-09-01 13:32:36 -0400

    adds app url prefix to build game url to play

--- app/src/app/environments/environment.ts
+++ app/src/app/environments/environment.ts
@@ -4,4 +4,5 @@ export const environment = {
   supabaseApiKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4cXZ5b3R5bGVzY3F4YWF5cWJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUyMDU3NjMsImV4cCI6MjA0MDc4MTc2M30.ypdZbKEQX7iZR8t6EoWFIJJj5sktkQGsNAt7kIjWkJk',
   supabaseProjectURL: 'https://hxqvyotylescqxaayqbl.supabase.co',
+  applicationLinkURLPreffix: 'http://localhost:4200',
 };
