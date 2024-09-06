b6ffa81e6b30491499153e7d1c956e30ebcb3261
Hamilton Lima <hamilton.lima@gmail.com>
2024-09-05 23:23:47 -0400

    send user details to mixpanel after login

--- app/src/app/components/common-page/common-page.component.ts
+++ app/src/app/components/common-page/common-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '@supabase/supabase-js';
-import { CurrentUserService } from '../../auth/current-user.service';
 import { FooterComponent } from '../../footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { CurrentUserService } from '../../auth/current-user.service';
 import { EventsService } from '../../events.service';
 
 @Component({
@@ -22,6 +22,9 @@ export class CommonPageComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     this.user = await this.currentUser.get();
+    if (this.user) {
+      this.events.people({ email: this.user.email });
+    }
     console.log('user', this.user);
   }
 
